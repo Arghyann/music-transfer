5db1d499dcec67ba1431735eada4a00a72b5c677
Arghyann <144401510+Arghyann@users.noreply.github.com>
2024-12-18 22:41:01 +0530

    fresh start 2

--- attempt1/demoProj/testapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class demoDB(models.Model):
-    name=models.CharField(max_length=200)
-    completed=models.BooleanField(default=False)
--- attempt1/demoProj/testapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-from .models import demoDB
-# Create your views here.
-def home(request):
-    return HttpResponse("what is a response")
-
-def todos(request):
-    items=demoDB.objects.all()
-    return render(request, "db.html",{"todos": items})
